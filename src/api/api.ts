0164c77878b2d1da1f34db6afecb0aece31bd79a
Komandr33 <and.kom2017@gmail.com>
2024-11-06 02:29:53 +0300

    add API

---
+++ src/api/api.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+  withCredentials: true,
+  headers: {
+    'API-KEY': '54674fe9-3989-480d-b07c-39c3a8b77a5a'
+  }
+})
+
+export const usersAPI = {
+  getUsers(currentPage: number, pageSize: number) {
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(res => res.data)
+  }
+}
+
+export const authAPI = {
+  getIsAuth() {
+    return instance.get(`auth/me`).then(res => res.data)
+  }
+}
+
+export const profileAPI = {
+  getProfile(userId: number) {
+    return instance.get(`profile/${userId}`).then(res=> res.data)
+  }
+}
+
+export const followAPI = {
+  addFollow(userId: number) {
+    return instance.post(`follow/${userId}`).then(res=> res.data)
+  },
+
+  deleteFollow(userId: number) {
+    return instance.delete(`follow/${userId}`).then(res=> res.data)
+  }
+}
---
+++ src/api/userAPI.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+//
+// const instance = axios.create({
+//   baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+//   withCredentials: true,
+//   headers: {
+//     'API-KEY': '5f947fcf-034a-41eb-9b43-8e8ef81acb52'
+//   }
+// })
+//
+// export const usersAPI = {
+//   getUsers(currentPage: number, pageSize: number) {
+//     return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(res => res.data)
+//   }
+// }
+
