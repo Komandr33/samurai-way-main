32feab5ea65365489dcc20ca089cefb370a32036
Komandr33 <and.kom2017@gmail.com>
2024-11-06 02:36:17 +0300

    add disabled button followed in User.tsx

--- src/components/Body/Users/User.tsx
+++ src/components/Body/Users/User.tsx
@@ -1,39 +1,39 @@
 import React from 'react';
-import user_avatar from '../../../asstets/images/user_avatar.jpg';
+import user_avatar from '../../../assets/images/user_avatar.jpg';
 import {UserType} from '../../../redux/users-reducer';
 import {Link} from 'react-router-dom';
 import {PATH} from '../../../App';
-import axios from 'axios';
+import {followAPI} from '../../../api/api';
 
 type UserPropsType = {
   user: UserType,
   toggleFollowed: (id: number) => void
+  toggleIsFollowingProgress: (isFetching: boolean, userId: number) => void
+  followedInProgress: number[]
 }
 export const User = (props: UserPropsType) => {
 
-  const authParams = {
-    withCredentials: true,
-    headers: {
-      'API-KEY': '0df2c3a9-0b40-49a2-a2b8-e325c30c4e8e'
-    }
-  }
-
-  const onClickHandler = () => {
+  const followedOnClickHandler = () => {
     if (!props.user.followed) {
-      axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${props.user.id}`, {}, authParams)
-        .then(res => {
-          if (res.data.resultCode == 0) {
+      props.toggleIsFollowingProgress(true, props.user.id)
+      followAPI.addFollow(props.user.id)
+        .then(data => {
+          if (data.resultCode == 0) {
+            props.toggleIsFollowingProgress(false, props.user.id)
             props.toggleFollowed(props.user.id)
           }
         })
-    } 
-    if (props.user.followed) axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${props.user.id}`, authParams)
-      .then(res => {
-        if (res.data.resultCode == 0) {
-          props.toggleFollowed(props.user.id)
-        }
-      })
-
+    }
+    if (props.user.followed) {
+      props.toggleIsFollowingProgress(true, props.user.id)
+      followAPI.deleteFollow(props.user.id)
+        .then(data => {
+          if (data.resultCode == 0) {
+            props.toggleIsFollowingProgress(false, props.user.id)
+            props.toggleFollowed(props.user.id)
+          }
+        })
+    }
   }
 
   return (
@@ -52,7 +52,8 @@ export const User = (props: UserPropsType) => {
           </button>
         </div>
         <div>
-          <button className={''} onClick={onClickHandler}>
+          <button className={''} onClick={followedOnClickHandler}
+                  disabled={props.followedInProgress.some(id => id === props.user.id)}>
             {!props.user.followed ? 'Добавить в друзья' : 'Удалить из друзей'}
           </button>
         </div>
